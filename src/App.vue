6cba05b9255a378d69ef74ca24da2deb2599d865
Ash Lawson <108193997+Ashyy-code@users.noreply.github.com>
2023-06-01 18:05:37 +0100

    Added return API functions

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
+  <div v-if="!this.$store.state.appLoaded">Loading data..</div>
   <div v-if="this.$store.state.appLoaded">
-  Data has finished loading now..
-  {{ this.$store.state.playerChars }}
-  {{ this.$store.state.dungeonList }}
-  {{ this.$store.state.affixList }}
-</div>
+    <button @click="getGroup()">Test the API</button>
+    <div v-if="groupCompData">
+      {{ groupCompData }}
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,33 +16,98 @@ import axios from "axios";
 export default {
   //on initializaion of main app..
   mounted() {
-    //check all resolved
-    let playersLoaded = this.loadPlayerChars().then(res => this.$store.state.playerChars = JSON.parse(JSON.parse(res.data.d).message));
-    let affixesLoaded = this.loadAffixList().then(res => this.$store.state.affixList = JSON.parse(JSON.parse(res.data.d).message));
-    let dungeonsLoaded = this.loadDungeonList().then(res => this.$store.state.dungeonList = JSON.parse(JSON.parse(res.data.d).message));
+    //check all resolved1
+    let playersLoaded = this.loadPlayerChars().then(
+      (res) =>
+        (this.$store.state.playerChars = JSON.parse(
+          JSON.parse(res.data.d).message
+        ))
+    );
+    let affixesLoaded = this.loadAffixList().then(
+      (res) =>
+        (this.$store.state.affixList = JSON.parse(
+          JSON.parse(res.data.d).message
+        ))
+    );
+    let dungeonsLoaded = this.loadDungeonList().then(
+      (res) =>
+        (this.$store.state.dungeonList = JSON.parse(
+          JSON.parse(res.data.d).message
+        ))
+    );
 
-    Promise.all([playersLoaded,affixesLoaded,dungeonsLoaded]).then(
-      res => {
-        this.$store.state.appLoaded = true;
-      }
-    )
+    //Only begin once all data is loaded
+    Promise.all([playersLoaded, affixesLoaded, dungeonsLoaded]).then((res) => {
+      this.$store.state.appLoaded = true;
+    });
   },
+
+  //V-model bindings here for the selected options
+  data() {
+    return {
+      //selected client items
+      selectedDungeon: null,
+      selectedKeyStoneLevel: 0,
+      selectedAffix1_id: 0,
+      selectedAffix2_id: 0,
+      selectedAffix3_id: 0,
+      selectedPlayerName1: null,
+      selectedPlayerName2: null,
+      selectedPlayerName3: null,
+      selectedPlayerName4: null,
+      selectedPlayerName5: null,
+
+      //returned Group Info from API
+      groupCompData: null,
+    };
+  },
+
   //Main app methods and stuff..
   methods: {
-
     //API call to the player Characters Google Doc
     async loadPlayerChars() {
-      return await axios.post(this.$store.state.webServiceURL + "playerChars",{contentType: "application/json"});
+      return await axios.post(this.$store.state.webServiceURL + "playerChars", {
+        contentType: "application/json",
+      });
     },
 
     //API call to the dungeon list
     async loadDungeonList() {
-      return await axios.post(this.$store.state.webServiceURL + "dungeonList",{contentType: "application/json"});
+      return await axios.post(this.$store.state.webServiceURL + "dungeonList", {
+        contentType: "application/json",
+      });
     },
 
     //API call to the Affix list
     async loadAffixList() {
-      return await axios.post(this.$store.state.webServiceURL + "affixList",{contentType: "application/json"});
+      return await axios.post(this.$store.state.webServiceURL + "affixList", {
+        contentType: "application/json",
+      });
+    },
+
+    //Submit the group to the API
+    async getGroup() {
+      await axios
+        .post(this.$store.state.webServiceURL + "computedGroup", {
+          contentType: "application/json",
+          dungeonName: this.selectedDungeon,
+          keystoneLevel: this.selectedKeyStoneLevel,
+          affix_1_id: this.selectedAffix1_id,
+          affix_2_id: this.selectedAffix2_id,
+          affix_3_id: this.selectedAffix3_id,
+          player_1_name: this.selectedPlayerName1,
+          player_2_name: this.selectedPlayerName2,
+          player_3_name: this.selectedPlayerName3,
+          player_4_name: this.selectedPlayerName4,
+          player_5_name: this.selectedPlayerName5,
+        })
+        .then((res) => {
+          let response = JSON.parse(res.data.d);
+          let response_status = response.status;
+          let response_data = response.message;
+          //check the status
+          if(response_status == "success"){this.groupCompData = JSON.parse(response.message)}
+        });
     },
   },
 };
